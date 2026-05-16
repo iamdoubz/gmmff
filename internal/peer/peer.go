c0d96571c368cf0fd235b08f2d76fa0243d822f0
iamdoubz <>
2026-05-16 15:26:46 -0500

    Add verbose ICE logging

--- internal/peer/peer.go
+++ internal/peer/peer.go
@@ -1881,9 +1881,6 @@ func newPeerConnection(cfg Config) (*webrtc.PeerConnection, error) {
 	pc.OnICEConnectionStateChange(func(s webrtc.ICEConnectionState) {
 		fmt.Printf("[ICE] connection state: %s\n", s)
 	})
-	pc.OnICEGatheringStateChange(func() {
-		fmt.Printf("[ICE] gathering state: %s\n", pc.ICEGatheringState())
-	})
 	pc.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
 		fmt.Printf("[PC]  connection state: %s\n", s)
 	})
